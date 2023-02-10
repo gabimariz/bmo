efea5e79d2098813c1bc3abd70b99330e7d245d1
Gabriel Mariz <gabriellucena997@outlook.com>
2023-02-09 23:12:48 -0300

    :sparkles: feat: new commands and Token env

--- src/main/java/me/gabimariz/bot/Main.java
+++ src/main/java/me/gabimariz/bot/Main.java
@@ -1,9 +1,9 @@
 package me.gabimariz.bot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import me.gabimariz.bot.commands.Live;
 import me.gabimariz.bot.commands.Social;
-import me.gabimariz.bot.commands.music.Pause;
-import me.gabimariz.bot.commands.music.Play;
+import me.gabimariz.bot.commands.music.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -11,16 +11,25 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import java.util.EnumSet;
 
 public class Main {
+  public static final Dotenv ENV = Dotenv.load();
 
-    public static JDA api;
-    private static final String TOKEN = "";
-    public static void main(String[] args) {
+  public static JDA api;
 
-         api = JDABuilder.createDefault(TOKEN, EnumSet.allOf(GatewayIntent.class))
-                 .addEventListeners(new Social(), new Live(), new Play(), new Pause())
-                 .build();
+  public static void main(String[] args) {
 
-         api.upsertCommand("social", "My social networks").queue();
-         api.upsertCommand("live", "live on").queue();
-    }
+    api = JDABuilder.createDefault(ENV.get("TOKEN"), EnumSet.allOf(GatewayIntent.class))
+      .addEventListeners(
+        new Social(),
+        new Live(),
+        new Play(),
+        new Pause(),
+        new Skip(),
+        new Stop(),
+        new NowPlaying()
+      )
+      .build();
+
+    api.upsertCommand("social", "My social networks").queue();
+    api.upsertCommand("live", "live on").queue();
+  }
 }
