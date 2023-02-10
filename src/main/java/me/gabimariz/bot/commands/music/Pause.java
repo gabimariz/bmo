4a1d466ef1b6b5c37ce70e947f9a772b15ff2453
Gabriel Mariz <gabriellucena997@outlook.com>
2023-02-09 23:15:08 -0300

    :recycle: refactor: verify

--- src/main/java/me/gabimariz/bot/commands/music/Pause.java
+++ src/main/java/me/gabimariz/bot/commands/music/Pause.java
@@ -3,33 +3,36 @@ package me.gabimariz.bot.commands.music;
 import me.gabimariz.bot.audio.lavaplayer.PlayerManager;
 import me.gabimariz.bot.utils.Embed;
 import me.gabimariz.bot.utils.MemberMessage;
+import me.gabimariz.bot.utils.Verify;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class Pause extends ListenerAdapter {
   @Override
-  public void onMessageReceived(MessageReceivedEvent event) {
-    String[] command = event.getMessage().getContentRaw().split(" ");
-
+  public void onMessageReceived(@NotNull MessageReceivedEvent event) {
     TextChannel channel = event.getChannel().asTextChannel();
 
-    if(event.getAuthor().isBot()) return;
+    if (Verify.isBot(event))
+      return;
 
-    if(command[0].equals("!pause")) {
-      if(MemberMessage.role(event, "")
-        || MemberMessage.permission(event, Permission.ADMINISTRATOR)) {
-        channel.sendMessageEmbeds(Embed.error()).queue();
+    if (Verify.command(event)[0].equals("!pause")) {
+      if (MemberMessage.role(event, "")
+          || MemberMessage.permission(event, Permission.ADMINISTRATOR)) {
+        channel.sendMessageEmbeds(Embed.status(
+            "Access denied",
+            new Color(217, 83, 79),
+            "You are not allowed to use the command!")).queue();
 
         return;
       }
 
-      if(PlayerManager.getInstance().getMusicManager(event.getGuild()).SCHEDULER.AUDIO_PLAYER.isPaused()) {
-        PlayerManager.getInstance().getMusicManager(event.getGuild()).SCHEDULER.AUDIO_PLAYER.setPaused(false);
-      } else {
-        PlayerManager.getInstance().getMusicManager(event.getGuild()).SCHEDULER.AUDIO_PLAYER.setPaused(true);
-      }
+      PlayerManager.getInstance().getMusicManager(event.getGuild()).SCHEDULER.AUDIO_PLAYER
+          .setPaused(!PlayerManager.getInstance().getMusicManager(event.getGuild()).SCHEDULER.AUDIO_PLAYER.isPaused());
     }
   }
 }
