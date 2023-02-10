00e44421106d23d94e6eb6d7782a32a44ea59674
Gabriel Mariz <gabriellucena997@outlook.com>
2023-02-09 23:14:09 -0300

    :recycle: refactor: embed social message

--- src/main/java/me/gabimariz/bot/commands/Social.java
+++ src/main/java/me/gabimariz/bot/commands/Social.java
@@ -1,5 +1,6 @@
 package me.gabimariz.bot.commands;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import me.gabimariz.bot.utils.EmbedMessage;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -7,24 +8,28 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import java.awt.*;
 
 public class Social extends ListenerAdapter {
-    @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-      EmbedMessage embed = new EmbedMessage(
-        "Other medias",
-        new Color(19, 102, 233),
-        "**[instagram]()** \n" +
-          "**[twitter]()**\n" +
-          "**[twitch]()**\n" +
-          "**[youtube]()**\n" +
-          "**[linkedin]()**\n" +
-          "**[github]()**",
-        "",
+  private final Dotenv ENV = Dotenv.load();
+
+  @Override
+  public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+    EmbedMessage embed = new EmbedMessage(
+      "Other medias",
+      new Color(19, 102, 233),
+      String.format(
+        "*[instagram](%s)**\n **[twitter](%s)**\n **[twitch](%s)**\n **[youtube](%s)**\n **[linkedin](%s)**\n **[github](%s)**",
+        ENV.get("INSTAGRAM_URL"),
+        ENV.get("TWITTER_URL"),
+        ENV.get("TWITCH_URL"),
+        ENV.get("YOUTUBE_URL"),
+        ENV.get("LINKEDIN_URL"),
+        ENV.get("GITHUB_URL")
+      ),
+      "",
       ""
       );
-
 
       if(event.getName().equals("social")) {
         event.replyEmbeds(embed.thumbnail().build()).queue();
       }
-    }
+  }
 }
